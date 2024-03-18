e29de262f91bed7f897329b519f88a678266eef5
root <root@v2.discours.io>
2024-03-18 19:43:01 +0100

    feat: change send_db

--- robo_sendDB.sh
+++ robo_sendDB.sh
@@ -5,7 +5,7 @@ export PATH="$PATH:/usr/local/sbin:/usr/sbin:/sbin"
 APP="discoursio-api"
 SSH_KEY="/root/.ssh/id_rsa"
 YMD=$(date "+%Y-%m-%d")
-DUMP_PATH="/var/lib/dokku/data/storage/core/dump/"
+DUMP_PATH="/var/lib/dokku/data/storage/migrator/dump/"
 DATA_PATH="/var/lib/dokku/data/storage/discoursio-api/migration/data"
 SCRIPT_PATH="/root/robo_script"
 MONGO_DB_PATH="/var/backups/mongodb"
@@ -16,7 +16,7 @@ NEW_DB="discoursio-db-new-$YMD"
 DSN_OLD_DB=$(dokku config:get "$APP" DATABASE_URL)
 LAST_DB_MONGO=$(find "$MONGO_DB_PATH" -printf '%T@ %p\n' | sort -nk1 | grep discours | tail -n 1 | /bin/awk '{print $2}')
 LAST_DB_POSTGRES=$(find "$POSTGRES_DB_PATH" -printf '%T@ %p\n' | sort -nk1 | grep discours | tail -n 1 | /bin/awk '{print $2}')
-NEW_HOST="5.255.97.70"
+NEW_HOST="5.255.122.72"
 NEW_PATH="/root/."
 NEW_PATH_MONGO="/var/lib/dokku/data/storage/migrator/dump/"
 
@@ -34,7 +34,7 @@ put_mongo_dump() {
 echo "put mongo.dump for core migration"
 # need to remove old dump, if exist
 if [ -d "$DUMP_PATH" ]; then
-rm -r "$DUMP_PATH"*
+    rm "$DUMP_PATH"/*.bson "$DUMP_PATH"/*.json "$DUMP_PATH"/*.tar.gz
 fi
 cp "$LAST_DB_MONGO" "$DUMP_PATH"
 chown -R 32767:32767 "$DUMP_PATH"
